df7135587091d6c6f6959b6ff7deabb0ad9ec3be
Inna Kirianova <innr@ya.ru>
2022-12-12 00:45:22 +0300

    added code for task52

---
+++ Task52/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите размер массива m x n. ");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 11);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("-----------------------");
+
+for (int j = 0; j < n; j++)
+{
+    double sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum+=matrix[i, j];
+    }
+    Console.Write((Math.Round(sum / m, 2)) + "\t");
+}
+
