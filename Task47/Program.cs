cf0df376ecd07a9601b62b06b63471ad3988f106
Inna Kirianova <innr@ya.ru>
2022-12-12 00:02:58 +0300

    added code for task47

---
+++ Task47/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите размер массива m x n. ");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double [m, n];
+for (int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i, j] = Math.Round(new Random().Next( 0, 10) + new Random().NextDouble(), 2);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
